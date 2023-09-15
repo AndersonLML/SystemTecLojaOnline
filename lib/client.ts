46562f856ba53358c6f49e1e1f6f994de4c3ac75
Anderson Leite <gtlt1999@gmail.com>
2023-09-14 21:50:46 -0300

    upda5e para tuncionar

--- lib/client.ts
+++ lib/client.ts
@@ -3,11 +3,10 @@ import imageUrlBuilder from "@sanity/image-url";
 
 //connect to sanity
 export const client = sanityClient({
-  projectId: "3c4n15ly",
+  projectId: "h1g7r4lc",
   dataset: "production",
   apiVersion: "2022-06-04",
   useCdn: true,
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
 
 //be able to use sanity images
